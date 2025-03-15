000f0a9be3aa473c8a20e7f5f3af2c0a919bf73d
Yannic Spreen-Ledebur <yannic.spreen-ledebur@plan-d.com>
2025-03-15 20:35:03 +0100

    Applied formatting

--- berlin-2025/gesetzscraper/change_evaluation.py
+++ berlin-2025/gesetzscraper/change_evaluation.py
@@ -10,12 +10,20 @@ def extract_text_diff(new_text: str, old_text: str) -> tuple[str, str]:
 
     # Find the common prefix
     start = 0
-    while start < len(old_words) and start < len(new_words) and old_words[start] == new_words[start]:
+    while (
+        start < len(old_words)
+        and start < len(new_words)
+        and old_words[start] == new_words[start]
+    ):
         start += 1
 
     # Find the common suffix
     end_old, end_new = len(old_words), len(new_words)
-    while end_old > start and end_new > start and old_words[end_old - 1] == new_words[end_new - 1]:
+    while (
+        end_old > start
+        and end_new > start
+        and old_words[end_old - 1] == new_words[end_new - 1]
+    ):
         end_old -= 1
         end_new -= 1
 
@@ -26,7 +34,9 @@ def extract_text_diff(new_text: str, old_text: str) -> tuple[str, str]:
     return diff_new, diff_old
 
 
-def compare_law_versions(df_new: pd.DataFrame, df_old: pd.DataFrame, only_output_diff: bool = True) -> None:
+def compare_law_versions(
+    df_new: pd.DataFrame, df_old: pd.DataFrame, only_output_diff: bool = True
+) -> None:
     """Compare two law versions and print the differences on a paragraph level."""
     paragraphs_new = set(df_new.paragraph_id.tolist())
     paragraphs_old = set(df_old.paragraph_id.tolist())
@@ -34,25 +44,30 @@ def compare_law_versions(df_new: pd.DataFrame, df_old: pd.DataFrame, only_output
     print("Die folgenden Paragraphen wurden hinzugefügt:")
     new_paragraphs = paragraphs_new - paragraphs_old
     for p in new_paragraphs:
-        print(f'{p}: {df_new.loc[df.paragraph_id == p, "text_content"].iloc[0]}')
+        print(f"{p}: {df_new.loc[df.paragraph_id == p, 'text_content'].iloc[0]}")
 
     print("\n\nDie folgenden Paragraphen sind nicht mehr vorhanden:")
     removed_paragraphs = paragraphs_old - paragraphs_new
     for p in removed_paragraphs:
-        print(f'{p}: {df_old.loc[df_old.paragraph_id == p, "text_content"].iloc[0]}')
+        print(f"{p}: {df_old.loc[df_old.paragraph_id == p, 'text_content'].iloc[0]}")
 
     print("\n\nBei den folgenden Paragraphen hat sich der Text verändert:")
     common_paragraphs = paragraphs_new & paragraphs_old
     for p in common_paragraphs:
-        if df_new.loc[df_new.paragraph_id == p, "text_content"].iloc[0] != df_old.loc[df.paragraph_id == p, "text_content"].iloc[0]:
+        if (
+            df_new.loc[df_new.paragraph_id == p, "text_content"].iloc[0]
+            != df_old.loc[df.paragraph_id == p, "text_content"].iloc[0]
+        ):
             old_text = df_old.loc[df_old.paragraph_id == p, "text_content"].iloc[0]
             new_text = df_new.loc[df_new.paragraph_id == p, "text_content"].iloc[0]
 
             if only_output_diff:
-                new_text, old_text = extract_text_diff(new_text=new_text, old_text=old_text)
+                new_text, old_text = extract_text_diff(
+                    new_text=new_text, old_text=old_text
+                )
             print(f"{p}:")
-            print(f'Alter Text: {old_text}')
-            print(f'\nNeuer Text: {new_text}')
+            print(f"Alter Text: {old_text}")
+            print(f"\nNeuer Text: {new_text}")
 
 
 if __name__ == "__main__":
@@ -63,14 +78,21 @@ if __name__ == "__main__":
     # Create mock previous version, with one paragraph removed, one added, and one changed
     df_modified = df.copy()
     ind_to_delete = 28
-    new_paragraph = pd.Series({
-        "paragraph_id": "§ 21",
-        "paragraph_title": "Datendialog",
-        "text_content": "Lorem Ipsum Dolor Sit Amet",
-    })
+    new_paragraph = pd.Series(
+        {
+            "paragraph_id": "§ 21",
+            "paragraph_title": "Datendialog",
+            "text_content": "Lorem Ipsum Dolor Sit Amet",
+        }
+    )
     df_modified = pd.concat([df_modified, new_paragraph.to_frame().T])
     df_modified.drop(ind_to_delete, inplace=True)
-    start_index = df_modified.iloc[0, -1].find("(4) Leistungsberechtigte nach Absatz 1 Nummer 5")
-    df_modified.iloc[0, -1] = df_modified.iloc[0, -1][:start_index] + "(4) Leistungsberechtigte nach Absatz 1 Nummer 5, denen bereits von einem anderen Mitgliedstaat der Europäischen Union oder von einem am Verteilmechanismus teilnehmenden Drittstaat im Sinne von § 1a Absatz 4 Satz 1 internationaler Schutz gewährt worden ist, haben keinen Anspruch auf Leistungen nach diesem Gesetz, wenn der internationale Schutz fortbesteht. 2 Hilfebedürftigen Ausländern, die Satz 1 unterfallen, werden bis zur Ausreise, längstens jedoch für einen Zeitraum von zwei Wochen, einmalig innerhalb von zwei Jahren nur eingeschränkte Hilfen gewährt, um den Zeitraum bis zur Ausreise zu überbrücken (Überbrückungsleistungen); die Zweijahresfrist beginnt mit dem Erhalt der Überbrückungsleistungen nach Satz 2. 3 Hierüber und über die Möglichkeit der Leistungen nach Satz 6 sind die Leistungsberechtigten zu unterrichten. 4 Die Überbrückungsleistungen umfassen die Leistungen nach § 1a Absatz 1 und nach § 4 Absatz 1 Satz 1 und Absatz 2. 5 Sie sollen als Sachleistung erbracht werden. 6 Soweit dies im Einzelfall besondere Umstände erfordern, werden Leistungsberechtigten nach Satz 2 zur Überwindung einer besonderen Härte andere Leistungen nach den §§ 3, 4 und 6 gewährt; ebenso sind Leistungen über einen Zeitraum von zwei Wochen hinaus zu erbringen, soweit dies im Einzelfall auf Grund besonderer Umstände zur Überwindung einer besonderen Härte und zur Deckung einer zeitlich befristeten Bedarfslage geboten ist. 7 Neben den Überbrückungsleistungen werden auf Antrag auch die angemessenen Kosten der Rückreise übernommen. 8 Satz 7 gilt entsprechend, soweit die Personen allein durch die angemessenen Kosten der Rückreise die in Satz 4 genannten Bedarfe nicht aus eigenen Mitteln oder mit Hilfe Dritter decken können. 9 Die Leistung ist als Darlehen zu erbringen."
+    start_index = df_modified.iloc[0, -1].find(
+        "(4) Leistungsberechtigte nach Absatz 1 Nummer 5"
+    )
+    df_modified.iloc[0, -1] = (
+        df_modified.iloc[0, -1][:start_index]
+        + "(4) Leistungsberechtigte nach Absatz 1 Nummer 5, denen bereits von einem anderen Mitgliedstaat der Europäischen Union oder von einem am Verteilmechanismus teilnehmenden Drittstaat im Sinne von § 1a Absatz 4 Satz 1 internationaler Schutz gewährt worden ist, haben keinen Anspruch auf Leistungen nach diesem Gesetz, wenn der internationale Schutz fortbesteht. 2 Hilfebedürftigen Ausländern, die Satz 1 unterfallen, werden bis zur Ausreise, längstens jedoch für einen Zeitraum von zwei Wochen, einmalig innerhalb von zwei Jahren nur eingeschränkte Hilfen gewährt, um den Zeitraum bis zur Ausreise zu überbrücken (Überbrückungsleistungen); die Zweijahresfrist beginnt mit dem Erhalt der Überbrückungsleistungen nach Satz 2. 3 Hierüber und über die Möglichkeit der Leistungen nach Satz 6 sind die Leistungsberechtigten zu unterrichten. 4 Die Überbrückungsleistungen umfassen die Leistungen nach § 1a Absatz 1 und nach § 4 Absatz 1 Satz 1 und Absatz 2. 5 Sie sollen als Sachleistung erbracht werden. 6 Soweit dies im Einzelfall besondere Umstände erfordern, werden Leistungsberechtigten nach Satz 2 zur Überwindung einer besonderen Härte andere Leistungen nach den §§ 3, 4 und 6 gewährt; ebenso sind Leistungen über einen Zeitraum von zwei Wochen hinaus zu erbringen, soweit dies im Einzelfall auf Grund besonderer Umstände zur Überwindung einer besonderen Härte und zur Deckung einer zeitlich befristeten Bedarfslage geboten ist. 7 Neben den Überbrückungsleistungen werden auf Antrag auch die angemessenen Kosten der Rückreise übernommen. 8 Satz 7 gilt entsprechend, soweit die Personen allein durch die angemessenen Kosten der Rückreise die in Satz 4 genannten Bedarfe nicht aus eigenen Mitteln oder mit Hilfe Dritter decken können. 9 Die Leistung ist als Darlehen zu erbringen."
+    )
 
     compare_law_versions(df_new=df, df_old=df_modified, only_output_diff=True)
--- berlin-2025/gesetzscraper/scraper.py
+++ berlin-2025/gesetzscraper/scraper.py
@@ -25,29 +25,41 @@ def scrape_law(url: str) -> tuple[pd.DataFrame, dict]:
     if document_number := root.attrib["doknr"]:
         metadata["document_number"] = document_number
     for child in root:
-        if (law_abbreviation := child.find('.//jurabk')) is not None:
+        if (law_abbreviation := child.find(".//jurabk")) is not None:
             metadata["law_abbreviation"] = law_abbreviation.text
 
         paragraph_id = child.find(".//enbez")
         if paragraph_id is None:  # Header row with no paragraph_id
             if (document_title := child.find(".//langue")) is not None:
                 metadata["document_title"] = document_title.text
-            if (new_state_comment := child.find(".//standangabe[standtyp='Neuf']/standkommentar")) is not None:
+            if (
+                new_state_comment := child.find(
+                    ".//standangabe[standtyp='Neuf']/standkommentar"
+                )
+            ) is not None:
                 metadata["new_state_comment"] = new_state_comment.text
-            if (current_state_comment := child.find(".//standangabe[standtyp='Stand']/standkommentar")) is not None:
+            if (
+                current_state_comment := child.find(
+                    ".//standangabe[standtyp='Stand']/standkommentar"
+                )
+            ) is not None:
                 metadata["current_state_comment"] = current_state_comment.text
             continue
         paragraph_id = paragraph_id.text
         paragraph_title = child.find(".//titel").text
         try:
-            text_content = "\n".join(" ".join(p.itertext()) for p in child.findall(".//text/Content/P"))
+            text_content = "\n".join(
+                " ".join(p.itertext()) for p in child.findall(".//text/Content/P")
+            )
         except TypeError:  # Occurs when there is no text
             text_content = ""
-        paragraphs.append({
-            "paragraph_id": paragraph_id,
-            "paragraph_title": paragraph_title,
-            "text_content": text_content
-        })
+        paragraphs.append(
+            {
+                "paragraph_id": paragraph_id,
+                "paragraph_title": paragraph_title,
+                "text_content": text_content,
+            }
+        )
 
     return pd.DataFrame(paragraphs), metadata
 
